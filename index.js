3c434c429660f7a6b793e3fa1c7bee8df12e9e53
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-02-18 23:00:53 -0300

    Primeiros passos com sockets

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection',(socket)=>{ // o parâmetro "socket" serve para eu rastrear o usuário em questão
+    console.log('Conectado(Servidor)');
+
+    io.emit('clientConnect','Estou me conectando a partir do método clientConnect do script do browser'); // Emite para todo mundo que acessa o browser, até mesmo quem acabou de acessar a página
+    socket.broadcast.emit('newUserClient', 'Um novo usuário se conectou!'); // Emite a mensagem para o cliente que já está na sessão para apresentar usuários novos.
+
+    socket.on('disconnect',()=> { // Detecta quando o usuário sai do app
+        console.log('Desconectado.');
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on localhost:3000');
+});
+
+/*
+    A partir do método emit e broadcast.emit podemos nos conectar e comunicar com o Front/Client
+*/
