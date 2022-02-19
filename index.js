b42b10947da184ffd1ec5f34c07af7a18bcc6ec0
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-02-18 23:36:49 -0300

    Inserindo na tela as mensagens dos usuários

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+const users = [];
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -16,11 +18,8 @@ app.get('/', (req, res) => {
 io.on('connection',(socket)=>{ // o parâmetro "socket" serve para eu rastrear o usuário em questão
     console.log('Conectado(Servidor)');
 
-    io.emit('clientConnect','Estou me conectando a partir do método clientConnect do script do browser'); // Emite para todo mundo que acessa o browser, até mesmo quem acabou de acessar a página
-    socket.broadcast.emit('newUserClient', 'Um novo usuário se conectou!'); // Emite a mensagem para o cliente que já está na sessão para apresentar usuários novos.
-
-    socket.on('disconnect',()=> { // Detecta quando o usuário sai do app
-        console.log('Desconectado.');
+    socket.on('chat message', (obj) => {
+        io.emit('chat message', obj); // Reenvinado pra validar no front
     });
 });
 
@@ -30,4 +29,12 @@ server.listen(3000, () => {
 
 /*
     A partir do método emit e broadcast.emit podemos nos conectar e comunicar com o Front/Client
+
+        io.emit('clientConnect','Estou me conectando a partir do método clientConnect do script do browser'); // Emite para todo mundo que acessa o browser, até mesmo quem acabou de acessar a página
+
+        socket.broadcast.emit('newUserClient', 'Um novo usuário se conectou!'); // Emite a mensagem para o cliente que já está na sessão para apresentar usuários novos.
+
+        socket.on('disconnect',()=> { // Detecta quando o usuário sai do app
+            console.log('Desconectado.');
+        });
 */
